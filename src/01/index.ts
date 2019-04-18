523fedf8a962f7075b14c763574f7a7bdc78f07a
lilinyi <LOVER12161@163.com>
2019-04-18 15:02:11 +0800

    feat: 🎸 实现拖拽功能

--- src/01/index.ts
+++ src/01/index.ts
@@ -1,1 +1,20 @@
-console.log(1)
+import { fromEvent } from "rxjs";
+import { switchMap, map, takeUntil } from "rxjs/operators";
+
+const box: HTMLDivElement = document.querySelector(".box");
+
+const mousedown = fromEvent<MouseEvent>(box, "mousedown");
+const mouseup = fromEvent<MouseEvent>(document, "mouseup");
+const mousemove = fromEvent<MouseEvent>(document, "mousemove");
+mousedown
+  .pipe(
+    switchMap(e =>
+      mousemove.pipe(
+        map(event => [event.clientX - e.offsetX, event.clientY - e.offsetY]),
+        takeUntil(mouseup)
+      )
+    )
+  )
+  .subscribe(([x, y]) => {
+    box.style.transform = `translate(${x}px,${y}px)`;
+  });
