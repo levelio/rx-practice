6d42f23bea54f872837d48cdfa53733fe186f260
zhiqianghe <simlesos@gmail.com>
2019-04-18 13:50:11 +0800

    feat: 🎸 第一题答案

--- src/01/index.ts
+++ src/01/index.ts
@@ -1,1 +1,48 @@
-console.log(1)
+import { fromEvent } from 'rxjs'
+import { map, switchMap, takeUntil } from 'rxjs/operators'
+
+const box = document.querySelector(".box");
+const mouseDown$ = fromEvent(box, "mousedown");
+const mouseMove$ = fromEvent(document, "mousemove");
+const mouseUp$ = fromEvent(document, "mouseup");
+
+mouseDown$.pipe(
+  map((event: MouseEvent) => ({
+    x: event.clientX,
+    y: event.clientY,
+    initPos: getTranslate(event.target)
+  })),
+  switchMap(pos => {
+    return mouseMove$.pipe(
+      map((event: MouseEvent) => ({
+        x: event.clientX - pos.x + pos.initPos.x,
+        y: event.clientY - pos.y + pos.initPos.y
+      })),
+      takeUntil(mouseUp$)
+    )
+  })
+).subscribe(pos => {
+  console.log(pos)
+  setTranslate(box, pos)
+})
+
+function getTranslate (element) {
+  const style = getComputedStyle(element)
+  const regExp = /matrix\((\d+,\s){4}(\d+),\s(\d+)/i
+  const result = style.transform.match(regExp)
+  if (result) {
+    return {
+      x: parseInt(result[2], 10),
+      y: parseInt(result[3], 10)
+    }
+  } else {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+}
+
+function setTranslate (element, pos) {
+  element.style.transform = `translate(${pos.x}px, ${pos.y}px)`
+}
