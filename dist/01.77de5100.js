de09f799a26779fd1884b4e2038bfea69156363c
lilinyi <LOVER12161@163.com>
2019-09-15 20:44:21 +0800

    feat: 🎸 原生js实现拖放列表排序

--- dist/01.77de5100.js
+++ dist/01.77de5100.js
@@ -15064,7 +15064,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52781" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59999" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- dist/02.80ee2152.js
+++ dist/02.80ee2152.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59999" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60184" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../.nvm/versions/node/v8.9.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/01.80ee2152.js.map
+//# sourceMappingURL=/02.80ee2152.js.map
